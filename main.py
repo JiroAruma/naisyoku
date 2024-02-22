1f6773bfd4c9e0879d64f24cd49670308017f152
JiroAruma <158024391+JiroAruma@users.noreply.github.com>
2024-02-22 13:01:01 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Request
+from linebot import WebhookParser, LineBotApi
+from linebot.models import TextSendMessage
+import datetime
+import os
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_parser = WebhookParser(LINE_CHANNEL_SECRET)
+app = FastAPI()
+
+
+@app.post('/')
+async def kkshk(request: Request):
+    # X-Line-Signature ヘッダーの値を取得
+    signature = request.headers.get('X-Line-Signature', '')
+
+    # request body から event オブジェクトを取得
+    events = line_parser.parse((await request.body()).decode('utf-8'), signature)
+
+    # 各イベントの処理（※1つの Webhook に複数の Webhook イベントオブジェクトが含まれる場合あるため）
+    for event in events:
+        if event.type != 'message':
+            continue
+        if event.message.type != 'text':
+            continue
+
+        # LINE パラメータの取得
+        line_user_id = event.source.user_id
+        line_message = event.message.text
+        line_bot_api.push_message(line_user_id, TextSendMessage(line_message))
+                
+        
+    # LINE Webhook サーバーへ HTTP レスポンスを返す
+    return 'ok'
